451d69e077996aae939fe93488775de6139464f9
Ramin Zare <zare88@fastmail.com>
2022-03-23 11:05:01 +0100

    New youtube channel slides

---
+++ slides/Qafele Omr twitter bot.mm
@@ -0,0 +1,394 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Live twitter bot" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="false" ID="ID_159340698" CREATED="1618139830900" MODIFIED="1618574079122"><hook NAME="MapStyle" background="#e0e0e2" zoom="1.948">
+    <properties edgeColorConfiguration="#721817ff,#fa9f42ff,#2b4162ff,#0b6e4fff,#721817ff,#fa9f42ff,#2b4162ff,#0b6e4fff" show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_BRANCHES"/>
+<node TEXT="Specification" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" POSITION="right" ID="ID_1947522972" CREATED="1618140407241" MODIFIED="1618573640218">
+<icon BUILTIN="full-1"/>
+<edge COLOR="#990000" WIDTH="1"/>
+<node TEXT="What do we want to do?" ID="ID_1982554358" CREATED="1618140810025" MODIFIED="1618573940003">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="How to create a twitter bot?" ID="ID_658458392" CREATED="1618140841017" MODIFIED="1618566578889"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://developer.twitter.com/en">https://developer.twitter.com/en</a>
+    </p>
+    <p>
+      <a href="https://twitter.com/qafele_omr">https://twitter.com/qafele_omr</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="Using twitter4j" ID="ID_1876423614" CREATED="1618141022825" MODIFIED="1618423804708"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      http://twitter4j.org/en/
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="What is the plan to write the bot?" ID="ID_1464282947" CREATED="1618167435000" MODIFIED="1618566374605">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="What is Heroku?" ID="ID_1249561642" CREATED="1618140816729" MODIFIED="1618423804709">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="How to deploy on Heroku" ID="ID_957860422" CREATED="1618140872537" MODIFIED="1618423804710">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="What is TDD?" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" POSITION="right" ID="ID_1309857818" CREATED="1618167637844" MODIFIED="1618573640218">
+<icon BUILTIN="full-2"/>
+<node TEXT="Definitions" ID="ID_34049823" CREATED="1618518550164" MODIFIED="1618649082957">
+<node TEXT="Resource" FOLDED="true" ID="ID_1523033290" CREATED="1618567293054" MODIFIED="1618567769260"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.oreilly.com/library/view/test-driven-java-development/9781783987429/">https://www.oreilly.com/library/view/test-driven-java-development/9781783987429/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Test driven Java Development" ID="ID_1258296569" CREATED="1618567446278" MODIFIED="1618567484638">
+<hook URI="Qafele%20Omr%20twitter%20bot_files/png_3817109041112457228.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="TDD is difficult to master" ID="ID_156963504" CREATED="1618518560043" MODIFIED="1618518561929"/>
+<node TEXT="Speeds up the time-to-market" ID="ID_255648386" CREATED="1618558102739" MODIFIED="1618558112375"/>
+<node TEXT="Enables easier refactoring" ID="ID_599451016" CREATED="1618558113890" MODIFIED="1618649082957"/>
+<node TEXT="helps to create better design" ID="ID_1388296124" CREATED="1618563312372" MODIFIED="1618563320597"/>
+<node TEXT="fosters looser coupling" ID="ID_370008181" CREATED="1618563321059" MODIFIED="1618563329045"/>
+</node>
+<node TEXT="Understanding TDD" FOLDED="true" ID="ID_769092772" CREATED="1618565800696" MODIFIED="1618565806202">
+<node TEXT="Red-Green-Refactor" FOLDED="true" ID="ID_1181052197" CREATED="1618565828872" MODIFIED="1618565836009">
+<node TEXT="RED" ID="ID_1729704627" CREATED="1618565806872" MODIFIED="1618565867392" COLOR="#ff0000">
+<font BOLD="false"/>
+</node>
+<node TEXT="GREEN" ID="ID_1211129680" CREATED="1618565810072" MODIFIED="1618565879776" COLOR="#46ff00">
+<font BOLD="false"/>
+</node>
+<node TEXT="REFACTOR" ID="ID_974256033" CREATED="1618565812104" MODIFIED="1618565900664" COLOR="#999900">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Steps" FOLDED="true" ID="ID_1206359158" CREATED="1618565913416" MODIFIED="1618565917977">
+<node TEXT="Write a test" ID="ID_1943985359" CREATED="1618565918792" MODIFIED="1618566643907">
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="Run all tests and confirm that the last one is failing" ID="ID_472430536" CREATED="1618565923528" MODIFIED="1618573142947">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Write the implementation code" ID="ID_1951254040" CREATED="1618565928744" MODIFIED="1618566647835">
+<icon BUILTIN="full-3"/>
+</node>
+<node TEXT="Run all tests" ID="ID_1066746937" CREATED="1618565937240" MODIFIED="1618566649466">
+<icon BUILTIN="full-4"/>
+</node>
+<node TEXT="Refactor" ID="ID_1114554796" CREATED="1618565941527" MODIFIED="1618566652227">
+<icon BUILTIN="full-5"/>
+</node>
+<node TEXT="Repeat" ID="ID_1952385169" CREATED="1618565943656" MODIFIED="1618573154859">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+<node TEXT="Diagram" FOLDED="true" ID="ID_399694207" CREATED="1618565995689" MODIFIED="1618566508501">
+<node TEXT="Red-Green-Refactor cycle" ID="ID_289582956" CREATED="1618566476846" MODIFIED="1618566541317">
+<hook URI="Qafele%20Omr%20twitter%20bot_files/png_14327553347040955867.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="In practice" FOLDED="true" ID="ID_99555126" CREATED="1618567525149" MODIFIED="1618570045246">
+<node TEXT="Speed is the key" ID="ID_1363496317" CREATED="1618567530860" MODIFIED="1618567531789"/>
+<node TEXT="It&apos;s not about testing" ID="ID_94274511" CREATED="1618567532669" MODIFIED="1618567537037"/>
+<node TEXT="Testing" FOLDED="true" ID="ID_693720550" CREATED="1618567568508" MODIFIED="1618567571773">
+<node TEXT="White box (Structural)" FOLDED="true" ID="ID_739966631" CREATED="1618567588060" MODIFIED="1618568511079">
+<node TEXT="Efficient if finding errors and problems" ID="ID_158092792" CREATED="1618568004666" MODIFIED="1618568013371"/>
+<node TEXT="might not find unimplemented or missing features" ID="ID_1462644416" CREATED="1618568033337" MODIFIED="1618568451128"/>
+<node TEXT="causing unwanted test failures when the code is refactored" ID="ID_540548051" CREATED="1618568480311" MODIFIED="1618568497320"/>
+<node TEXT="some form like TDD" ID="ID_1128045862" CREATED="1618568501190" MODIFIED="1618568507143"/>
+</node>
+<node TEXT="Black box (Functional testing)" FOLDED="true" ID="ID_1124293697" CREATED="1618567571774" MODIFIED="1618567685900">
+<node TEXT="Good For large segments" ID="ID_1885130066" CREATED="1618567692444" MODIFIED="1618567908667"/>
+<node TEXT="Doesn&apos;t need code access and knowledge" ID="ID_636978757" CREATED="1618567910171" MODIFIED="1618568032123"/>
+<node TEXT="Limited and Blind coverage" ID="ID_1146805432" CREATED="1618567700379" MODIFIED="1618567745516"/>
+<node TEXT="Inefficient testing" ID="ID_772254609" CREATED="1618567888954" MODIFIED="1618567904763"/>
+<node TEXT="some form such as BDD" ID="ID_1117336273" CREATED="1618567707899" MODIFIED="1618567722012"/>
+</node>
+</node>
+<node TEXT="Mocking" FOLDED="true" ID="ID_1337786066" CREATED="1618569032900" MODIFIED="1618569035332">
+<node TEXT="Increase the speed of test" ID="ID_380491117" CREATED="1618569035652" MODIFIED="1618569072756"/>
+<node TEXT="removal external factors" ID="ID_530609580" CREATED="1618569076323" MODIFIED="1618569082452"/>
+</node>
+<node TEXT="Executable documentation" FOLDED="true" ID="ID_1929799314" CREATED="1618569192274" MODIFIED="1618569198531">
+<node TEXT="Code is bible" ID="ID_70019329" CREATED="1618569425248" MODIFIED="1618569731839">
+<font BOLD="true"/>
+</node>
+<node TEXT="the key is avoiding duplication" ID="ID_1668865093" CREATED="1618569438881" MODIFIED="1618569461633"/>
+<node TEXT="For other cases a simple README is enough" ID="ID_1992216062" CREATED="1618569639375" MODIFIED="1618569651040"/>
+<node TEXT="TDD adds more information" FOLDED="true" ID="ID_1800924702" CREATED="1618569710351" MODIFIED="1618569717904">
+<node TEXT="for non-coders BDD provides documentation" ID="ID_575164326" CREATED="1618569781374" MODIFIED="1618569796046">
+<font ITALIC="true"/>
+</node>
+</node>
+</node>
+<node TEXT="No debugging" ID="ID_1758020818" CREATED="1618570046541" MODIFIED="1618570049853"/>
+</node>
+</node>
+<node TEXT="Common Pitfalls" ID="ID_1424172811" CREATED="1619791489708" MODIFIED="1619791951542">
+<node TEXT="Time-intensive" ID="ID_18428051" CREATED="1619791957513" MODIFIED="1619791964486"/>
+<node TEXT="Less flexibility" ID="ID_1862900315" CREATED="1619791965259" MODIFIED="1619791973132"/>
+<node TEXT="Not a perfect solution" ID="ID_59090404" CREATED="1619791973673" MODIFIED="1619791980030"/>
+</node>
+</node>
+<node TEXT="JUnit" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" POSITION="right" ID="ID_1316412181" CREATED="1618517355838" MODIFIED="1618573640217">
+<icon BUILTIN="full-3"/>
+<node TEXT="simple and easy-to-learn framework for writing and running tests" ID="ID_827083307" CREATED="1618571527986" MODIFIED="1618571528963"/>
+<node TEXT="Implementation" FOLDED="true" ID="ID_698572049" CREATED="1618571704402" MODIFIED="1618572424845">
+<node TEXT="methods" ID="ID_1822961789" CREATED="1618571714065" MODIFIED="1618571715585">
+<node TEXT="@BeforeClass" ID="ID_186098923" CREATED="1618571717505" MODIFIED="1618571747969"/>
+<node TEXT="@Before" ID="ID_1078953623" CREATED="1618571718689" MODIFIED="1618571724786"/>
+<node TEXT="@Test" ID="ID_996380530" CREATED="1618571728273" MODIFIED="1618571733905">
+<font BOLD="true"/>
+</node>
+<node TEXT="@AfterClass" ID="ID_1643714230" CREATED="1618571737969" MODIFIED="1618571767745"/>
+<node TEXT="@After" ID="ID_1367271867" CREATED="1618571768064" MODIFIED="1618571770913"/>
+</node>
+<node TEXT="Assertion" ID="ID_407798152" CREATED="1618571980527" MODIFIED="1618571982591">
+<node TEXT="Assert.assert*" ID="ID_1016997333" CREATED="1618571983455" MODIFIED="1618572050939">
+<node TEXT="Assert.assertEquals(5, friendships.getFriendsList(&quot;Joe&quot;).size());" ID="ID_880171202" CREATED="1618572102670" MODIFIED="1618572376621">
+<font NAME="FreeMono"/>
+</node>
+</node>
+<node TEXT="Alternatives" FOLDED="true" ID="ID_1240668539" CREATED="1618572081614" MODIFIED="1618572085471">
+<node TEXT="Hamcrest assert" ID="ID_274193686" CREATED="1618572086350" MODIFIED="1618572087983">
+<node TEXT="assertThat(friendships.getFriendsList(&quot;Joe&quot;), hasSize(5));" ID="ID_1544941461" CREATED="1618572149966" MODIFIED="1618572158464">
+<font NAME="FreeMono"/>
+</node>
+</node>
+<node TEXT="AssertJ" ID="ID_1089761660" CREATED="1618572088398" MODIFIED="1618572090527">
+<node TEXT="assertThat(friendships.getFriendsList(&quot;Joe&quot;))&#xa;        .hasSize(5)&#xa;        .containsOnly(&quot;Audrey&quot;, &quot;Peter&quot;,&quot;Michael&quot;, &quot;Britney&quot;, &quot;Paul&quot;);" ID="ID_1750395278" CREATED="1618572192845" MODIFIED="1618572232511">
+<font NAME="FreeMono"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="JUnit5" FOLDED="true" ID="ID_1169000179" CREATED="1618572360268" MODIFIED="1618653018194"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://junit.org/junit5/docs/current/user-guide/">https://junit.org/junit5/docs/current/user-guide/ </a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="@ParameterizedTest&#xa;@ValueSource(ints = {})" ID="ID_221791328" CREATED="1618653010235" MODIFIED="1618653901909">
+<font SIZE="8" BOLD="false"/>
+</node>
+<node TEXT="Assertions.assertThrows" ID="ID_1247838926" CREATED="1618653128067" MODIFIED="1618653909770"/>
+<node TEXT="@DisplayName" ID="ID_1099052306" CREATED="1618653009086" MODIFIED="1618653853999"/>
+</node>
+</node>
+<node TEXT="Java Code Coverage (JaCoCo)" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" FOLDED="true" POSITION="right" ID="ID_89275617" CREATED="1618572282317" MODIFIED="1619560037038">
+<icon BUILTIN="full-4"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      survey of multiple studies on the impact of TDD has found that it&nbsp;<a href="https://medium.freecodecamp.org/8-observations-on-test-driven-development-a9b5144f868">reduces defects by 40 to 60 percent</a>, while increasing effort and execution time by 15 to 35 percent
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="tool for measuring test coverage" ID="ID_245783440" CREATED="1618572488700" MODIFIED="1618572634991"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.eclemma.org/jacoco/">https://www.eclemma.org/jacoco/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="What parts of our code are properly tested?" ID="ID_346583975" CREATED="1618572464924" MODIFIED="1618572465468"/>
+<node TEXT="It does not provide information about quality" ID="ID_288410271" CREATED="1618572562619" MODIFIED="1618655530396"/>
+<node TEXT="Report" FOLDED="true" ID="ID_1533713973" CREATED="1618572576475" MODIFIED="1618572578811">
+<node TEXT="A sample report" ID="ID_161628853" CREATED="1618572584256" MODIFIED="1618572645963">
+<hook URI="Qafele%20Omr%20twitter%20bot_files/png_483409409875269716.png" SIZE="0.65717417" NAME="ExternalObject"/>
+</node>
+</node>
+<node ID="ID_1796690294" CREATED="1619791970446" MODIFIED="1619791970446"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <h4 http-equiv="content-type" content="text/html; charset=utf-8">
+      Less flexibility
+    </h4>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Mockito" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" POSITION="right" ID="ID_125393679" CREATED="1618167651717" MODIFIED="1618655525095">
+<icon BUILTIN="full-5"/>
+<node TEXT="a mock, is a simulated object that can be used to replace real ones" ID="ID_422126529" CREATED="1618572740346" MODIFIED="1618572741114"/>
+<node TEXT="Mockito easy creation of the test double" ID="ID_584341472" CREATED="1618573000616" MODIFIED="1618573010041"/>
+<node TEXT="implementation" ID="ID_1324638201" CREATED="1618572826841" MODIFIED="1618572844841">
+<node ID="ID_1438482041" CREATED="1618572830985" MODIFIED="1618572887213"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>@Mock</b>
+    </p>
+    <p>
+      FriendsCollection friends;
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="FreeMono"/>
+</node>
+<node ID="ID_1275666508" CREATED="1618572845845" MODIFIED="1618572893409"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Person joe = new Person(&quot;Joe&quot;);
+    </p>
+    <p>
+      <b>doReturn(joe).when(friends).findByName(&quot;Joe&quot;);</b>
+    </p>
+    <p>
+      assertThat(friends.findByName(&quot;Joe&quot;)).isEqualTo(joe);
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="FreeMono"/>
+</node>
+<node TEXT="Alternatives" ID="ID_669939621" CREATED="1618572914488" MODIFIED="1618572922889">
+<node TEXT="EasyMock" ID="ID_963628421" CREATED="1618572923448" MODIFIED="1618572926601"/>
+</node>
+</node>
+</node>
+<node TEXT="Deploy on Heroku" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" POSITION="right" ID="ID_270259575" CREATED="1618140109710" MODIFIED="1618573640214">
+<icon BUILTIN="full-6"/>
+<edge COLOR="#990000" WIDTH="1"/>
+<node TEXT="Create an app" ID="ID_1846103816" CREATED="1618140179867" MODIFIED="1618423804713">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="Using heroku CLI to deploy" ID="ID_1485320734" CREATED="1618140188811" MODIFIED="1618423804714">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="Talking about web,worker,scheduler process" ID="ID_732054514" CREATED="1618140206010" MODIFIED="1618423804714">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="Using VARS" ID="ID_559628358" CREATED="1618140442906" MODIFIED="1618423804715">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node TEXT="change to heroku github" ID="ID_1715032557" CREATED="1618140318858" MODIFIED="1618423804715">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
